49dd85d1bc0f92fe2c39d85cfdc762ab4986f54b
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-16 13:17:06 +0900

    Inner-Class test 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+class Outer {
+    private class HiddenInner {
+        void secret() {
+            System.out.println("private!");
+        }
+    }
+
+    public void reveal() {
+        HiddenInner hi = new HiddenInner();
+        hi.secret();
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+    }
+}
 
 
+
+
+
+
+
+/* 3
 class Outer {
 
 }
@@ -21,7 +44,7 @@ public class Main {
         inner.showMessage();
     }
 }
-
+*/
 
 
 
