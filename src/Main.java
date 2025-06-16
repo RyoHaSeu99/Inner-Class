8dbfc0ed36fcd5615dccf2c86f0c2286f8a159a2
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-16 11:34:01 +0900

    Inner-Class test 2

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+class Car {
+    String brand;
+    Engine engine;
+
+    public Car(String brand) {
+        this.brand = brand;
+        this.engine = new Engine();
+    }
+
+    class Engine {
+        public void start() {
+            System.out.println("The engine of " + brand + " is starting.");
+        }
+    }
+
+    public void startEngine() {
+        engine.start();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("빠른자동차");
+
+        car.startEngine();
+    }
+}
+
+
+
+
+
+/* 1.
+public class Outer {
+    private String message = "Hello from the outer class.";
+
+    class InnerClass {
+        void displayMessage() {
+            System.out.println("Inner says: " + message);
+        }
+    }
+
+    public void callInner() {
+        InnerClass inner = new InnerClass();
+        inner.displayMessage();
+    }
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+
+        outer.callInner();
+
+    }
+}
+ */
